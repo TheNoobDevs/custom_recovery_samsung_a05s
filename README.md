c49a45662a06c150d7cdacd89e20491a106cf35f
MrFluffyOven <thenoobdevs2024@gmail.com>
2024-06-08 14:07:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for samsung SM-A057F (a05s)
+
+# Clone
+    git clone https://github.com/TheNoobDevs/custom_twrp_samsung_a057f.git -b twrp-12.1 device/samsung/a05s
+
+# Build
+    ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch twrp_a05s-eng; mka recoveryimage
+# Contributors
+- [FathanIhdhar296](https://github.com/FathanIhdhar296)
---
+++ device.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a05s
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd   
+
+# Recovery modules/dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libdmabufheap \
+    libion \
+    libxml2 \
+    libnetutils \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0 \
+    android.hardware.health-V1-ndk
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.health-V1-ndk.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so 
+
+# QCOM
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
