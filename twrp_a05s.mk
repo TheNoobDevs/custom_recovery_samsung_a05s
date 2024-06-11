5fc7a8d0a1fe593a559c4087b1e458fe23b6702e
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-06-11 11:26:50 +0100

    Disable Emulated

--- twrp_a05s.mk
+++ twrp_a05s.mk
@@ -6,7 +6,7 @@
 #
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
