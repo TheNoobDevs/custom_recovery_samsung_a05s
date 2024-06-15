607f4d8fc8b2a6959fb03d85e217fa6a46a6c8ed
MrFluffyOven <thenoobdevs2024@gmail.com>
2024-06-15 12:45:13 +0100

    include fastbootd

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock  
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
