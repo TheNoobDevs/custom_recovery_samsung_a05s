e1d3d6e20278a8c2cd9a74708c53bab044eea1c4
MrFluffyOven <thenoobdevs2024@gmail.com>
2024-06-17 13:35:51 +0100

    remove health blob that was physically added

--- device.mk
+++ device.mk
@@ -32,14 +32,12 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libnetutils \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
-    android.hardware.health-V1-ndk
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.health-V1-ndk.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so 
 
